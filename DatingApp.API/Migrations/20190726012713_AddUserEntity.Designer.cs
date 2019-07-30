35bfa122dbf077d0ab146072c0ff1329fc6efd5a
Iliess67 <el-moumene@hotmail.com>
2019-07-30 11:53:41 -0400

    FrontEnd Registration - Section 4

--- DatingApp.API/Migrations/20190726012713_AddUserEntity.Designer.cs
+++ DatingApp.API/Migrations/20190726012713_AddUserEntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190726012713_AddUserEntity")]
+    partial class AddUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -21,9 +23,9 @@ namespace DatingApp.API.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
 
-                    b.Property<byte[]>("PasswordHash");
+                    b.Property<byte[]>("PassewordHash");
 
-                    b.Property<byte[]>("PasswordSalt");
+                    b.Property<byte[]>("PassewordSalt");
 
                     b.Property<string>("Username");
 
